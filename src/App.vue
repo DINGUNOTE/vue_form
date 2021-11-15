22f6fa6d8ff7144baa341ad7d66e9493455ef6a2
DINGU <web.ljg@gmail.com>
2021-11-15 21:14:46 +0900

    로그인 form 프로젝트 완료

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <form action="">
+  <form v-on:submit.prevent="submitForm"> <!-- .prevent == .preventDefault();와 동일하게 작동 -->
     <div>
       <label for="username">id: </label>
-      <input type="text" id="username">
+      <input type="text" id="username" v-model="username"> <!-- username으로 입력받은 값을 username에 v-model을 이용해서 데이터 연결 -->
     </div>
     <div>
       <label for="password">pw: </label>
-      <input type="password" id="password">
+      <input type="password" id="password" v-model="password"> <!-- password으로 입력받은 값을 password에 v-model을 이용해서 데이터 연결 -->
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data: function(){
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function(){
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        });
+    }
+  }
 }
 </script>
 
